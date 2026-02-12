608084aa6a56f2888e001f1b2b8fa9b2e0f8e669
Шамшин Алексей <akshamshin@elavt.spb.ru>
2026-02-12 22:55:04 +0300

    [test_task2] Добавлены функции чтения для Поступлений. добавлены endpoints. Но к сервисам не подключены, пока все таак и работает на Балансе.

--- StockFlow.Application/ReadModel/ReceiptItemDto.cs
+++ StockFlow.Application/ReadModel/ReceiptItemDto.cs
@@ -3,8 +3,8 @@ namespace StockFlow.Application.ReadModel;
 public class ReceiptItemDto {
     public Guid Id { get; set; }
     public Guid ResourceId { get; set; }
-    public string ResourceName { get; set; }
+    public string ResourceName { get; set; } = string.Empty;
     public Guid UnitId { get; set; }
-    public string UnitName { get; set; }
+    public string UnitName { get; set; } = string.Empty;
     public decimal Quantity { get; set; }
 }
